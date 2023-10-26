625fc878d7b3265c26db24be2811b03d7ca65ba6
Nicolas Sauvage <nicolas.sauvage@renaissance.com>
2023-10-26 22:24:37 +0200

    add elysia

--- ts-back/src/index.ts
+++ ts-back/src/index.ts
@@ -3,29 +3,25 @@ import cors from "cors";
 import express from "express";
 import morgan from "morgan";
 
-import { Election } from "./domain/Election";
-import { createElection } from "./usecase/CreateElection";
-import { listElections } from "./usecase/ListElections";
 import lowdb from "lowdb";
 import { LowdbElectionRepo } from "./adapters/LowdbElectionRepo";
-import { Ballot } from "./domain/Ballot";
-import { saveVote } from "./usecase/SaveVote";
 import { LowdbVoteRepo } from "./adapters/LowdbVoteRepo";
-import { getResults } from "./usecase/GetResults";
-import { getElection } from "./usecase/getElection";
-import { findElectionByCode } from "./usecase/findElectionByCode";
 import { createRoutes } from "./router";
 
-const app = express();
+import { Elysia } from "elysia";
 
-app.use(morgan("tiny"));
+const app = new Elysia()
 
-app.use(bodyParser.json());
-app.use(cors());
+app.get('/', () => "Hello world!")
 
-app.get("/health", (req, res: express.Response) => {
-  res.status(200).send("UP");
-});
+// app.use(morgan("tiny"));
+
+// app.use(bodyParser.json());
+// app.use(cors());
+
+// app.get("/health", (req, res: express.Response) => {
+//   res.status(200).send("UP");
+// });
 
 app.listen(process.env.PORT || 8000, () => {
   console.log("Consensus server listening on port " + process.env.PORT || 8000);
@@ -48,7 +44,7 @@ const voteRepo = new LowdbVoteRepo(db);
 
 // Routes
 const router = createRoutes(electionRepo, voteRepo);
-app.use("/api/v1", router);
+// app.use("/api/v1", router);
 
 function initDb(db) {
   db.defaults({ elections: [], votes: [] }).write();
